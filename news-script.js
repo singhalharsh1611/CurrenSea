f30cd9269d85e4a48d4ca0de44b59f1c894b0ea1
HARSH SINGHAL <70061407+singhalharsh1611@users.noreply.github.com>
2023-11-13 11:44:45 +0530

    Update news-script.js

--- news-script.js
+++ news-script.js
@@ -1,27 +1,42 @@
-let key = "fc64d833cc2f4873a21b42aa3a915b58";
-let cardData = document.querySelector(".cardData")
+let key = "0867e42d37c44104949006bea1beba61";
+let cardData = document.querySelector(".cardData");
+let page = 1;
+
 const getData = async () => {
-    let response = await fetch(`https://newsapi.org/v2/everything?q=currency-india&sortBy=publishedAt&pageSize=75&apiKey=${key}`);
+    let response = await fetch(`https://newsapi.org/v2/everything?q=currencies&sortBy=publishedAt&pageSize=75&page=${page}&apiKey=${key}`);
     let jsonData = await response.json();
     console.log(jsonData.articles);
 
     jsonData.articles.forEach(function (article) {
         console.log(article);
 
-
         let divs = document.createElement("div");
         divs.classList.add("card");
         cardData.appendChild(divs);
         divs.innerHTML = `
-    <img src="${article.urlToImage}" alt="">
-    <h3>${article.title}</h3>
-    <p>${article.description}</p>
-    `
+            <img src="${article.urlToImage}" alt="">
+            <h3>${article.title}</h3>
+            <p>${article.description}</p>
+        `;
 
         divs.addEventListener("click", function () {
             window.open(article.url);
-        })
-    })
+        });
+    });
+};
 
-}
 getData();
+
+document.querySelector("#next").addEventListener("click", function () {
+    page++;
+    cardData.innerHTML = "";
+    getData();
+});
+
+document.querySelector("#prev").addEventListener("click", function () {
+    if (page > 1) {
+        page--;
+        cardData.innerHTML = "";
+        getData();
+    }
+});
